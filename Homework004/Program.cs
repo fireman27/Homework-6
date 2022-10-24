eedfca78ef8de59cc5fcab55670429976b03bbde
fireman <fireman2747@yandex.ru>
2022-10-24 19:57:58 +0800

    add Homework 004 копия массива добавил описание

--- Homework004/Program.cs
+++ Homework004/Program.cs
@@ -1,31 +1,30 @@
 ﻿// Написать программу копирования массива
 
-
 int[] GetArray(int size, int min, int max)
 {
-    int[] arr = new int[size];
+    int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = new Random().Next(min, max + 1);
+        array[i] = new Random().Next(min, max + 1);
     }
-    return arr;
+    return array;
 }
 
-int[] CopyArray(int[] originArray)
+int[] CopyArray(int[] mirrorarray)
 {
-    int[] copy = new int[originArray.Length];
-    for (int i = 0; i < originArray.Length; i++)
+    int[] copy = new int[mirrorarray.Length];
+    for (int i = 0; i < mirrorarray.Length; i++)
     {
-        copy[i] = originArray[i];
+        copy[i] = mirrorarray[i];
     }
     return copy;
 }
 
-void PrintArray(int[] arr)
+void PrintArray(int[] array)
 {
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        System.Console.Write(arr[i] + " ");
+        Console.Write(array[i] + " ");
     }
 }
 
@@ -33,10 +32,10 @@ int size = 10;
 int min = 1;
 int max = 100;
 
-int[] origin = GetArray(size, min, max);
-System.Console.WriteLine("Исходный массив А:");
-PrintArray(origin);
-System.Console.WriteLine();
-System.Console.WriteLine("Копия массива А:");
-int[] copy = CopyArray(origin);
+int[] mirror = GetArray(size, min, max);
+Console.WriteLine("Исходный массив А:");
+PrintArray(mirror);
+Console.WriteLine();
+Console.WriteLine("Копия массива А:");
+int[] copy = CopyArray(mirror);
 PrintArray(copy);
